8322c928ca65d54d33fbbfc4b8bb9a85de8e1920
Marina Karpukhina <marina.ks.developer@gmail.com>
2018-05-25 17:40:47 +0300

    [data-grid-core] table-cell, table-layout

---
+++ src/app/screens/ermodel/components/data-grid-core/table/TableCell.tsx
@@ -0,0 +1,32 @@
+import React, { ReactChild, ReactType } from 'react';
+
+interface TableCellProps {
+  children?: ReactChild | ReactChild[];
+  loading?: boolean; // content = fixed-height skeleton
+  renderComponent: ReactType; // 'td' // TODO func({key, rowData, column})
+  renderContent: ReactType;
+  renderContentSkeleton?: ReactType;
+  // ...componentProps:
+  // tabIndex: number, // browser-focusable
+  // onDoubleClick: ()=>void
+}
+
+class TableCell extends React.Component<TableCellProps, any> {
+  public static defaultProps = {
+    loading: false
+  };
+
+  public render(): JSX.Element {
+    const {
+      renderComponent: Component,
+      renderContent: Content,
+      renderContentSkeleton: Skeleton,
+      loading,
+      ...componentProps
+    } = this.props;
+
+    return <Component {...componentProps}>{loading && Skeleton ? <Skeleton /> : <Content />}</Component>;
+  }
+}
+
+export { TableCell, TableCellProps };
---
+++ src/app/screens/ermodel/components/data-grid-core/table/TableLayout.tsx
@@ -0,0 +1,109 @@
+import React, { Component, Key, ReactType } from 'react';
+
+interface TableColumn {
+  id: Key;
+  widthPx?: number;
+  // align: string
+}
+
+interface TableLayoutProps {
+  bodyRows?: any[];
+  columns: TableColumn[];
+  footRows?: any[];
+  headRows?: any[];
+  minColumnWidthPx: number;
+  renderBody: ReactType;
+  renderBodyCell: ReactType;
+  renderColGroup: ReactType; // 'colgroup',
+  renderColGroupCol: ReactType; // 'col'
+  renderFoot: ReactType;
+  renderFootCell: ReactType;
+  renderHead: ReactType;
+  renderHeadCell: ReactType;
+  renderRow: ReactType;
+  renderTable: ReactType;
+}
+
+// TODO arrow -> renderItem
+// TODO col, cell types
+
+class TableLayout extends Component<TableLayoutProps, any> {
+  public static defaultProps = {
+    bodyRows: [],
+    footRows: [],
+    headRows: []
+  };
+
+  constructor(props: TableLayoutProps) {
+    super(props);
+
+    this.getMinWidth = this.getMinWidth.bind(this);
+  }
+
+  private getMinWidth(columns: TableColumn[]): number {
+    const { minColumnWidthPx } = this.props;
+
+    return columns.map(column => column.widthPx || minColumnWidthPx || 0).reduce((acc, width) => acc + width, 0) || 0;
+  }
+
+  public render(): JSX.Element {
+    const {
+      columns,
+      headRows,
+      bodyRows,
+      footRows,
+      renderTable: Table,
+      renderHead: Head,
+      renderBody: Body,
+      renderFoot: Foot,
+      renderRow: Row,
+      renderHeadCell: HeadCell,
+      renderBodyCell: BodyCell,
+      renderFootCell: FootCell,
+      renderColGroup: ColGroup,
+      renderColGroupCol: Col
+    } = this.props;
+
+    const minWidth = this.getMinWidth(columns);
+
+    return (
+      <Table style={{ minWidth: `${minWidth}px` }}>
+        {ColGroup && (
+          <ColGroup>
+            {columns.map(column => <Col key={column.id} style={{ width: column.widthPx }} column={column} />)}
+          </ColGroup>
+        )}
+        {Head && (
+          <Head>
+            {!!headRows &&
+              headRows.map(row => (
+                <Row key={row.id}>
+                  {columns.map(column => <HeadCell key={column.id} column={column} rowData={row} />)}
+                </Row>
+              ))}
+          </Head>
+        )}
+        <Body>
+          {!!bodyRows &&
+            bodyRows.map(row => (
+              <Row key={row.id}>
+                {columns.map(column => <BodyCell key={column.id} column={column} rowData={row} />)}
+              </Row>
+            ))}
+        </Body>
+        {Foot && (
+          <Foot>
+            {!!footRows &&
+              footRows.map(row => (
+                <Row key={row.id}>
+                  {columns.map(column => <FootCell key={column.id} column={column} rowData={row} />)}
+                </Row>
+              ))}
+          </Foot>
+        )}
+      </Table>
+    );
+  }
+}
+
+export { TableLayout, TableLayoutProps, TableColumn };
