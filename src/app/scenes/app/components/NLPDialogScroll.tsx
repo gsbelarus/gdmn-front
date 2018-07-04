74b5075f6a4f133077597437e6a7f5d02a2bbf34
andreik <gs1994@gmail.com>
2018-07-04 20:14:39 +0300

    telegram like dialog style

--- src/app/scenes/app/components/NLPDialogScroll.tsx
+++ src/app/scenes/app/components/NLPDialogScroll.tsx
@@ -24,7 +24,7 @@ export class NLPDialogScroll extends Component<INLPDialogScrollProps, INLPDialog
   }
 
   onPressEnter(e: React.KeyboardEvent<HTMLTextAreaElement>) {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && this.state.text.trim()) {
       this.props.onSetText(this.state.text);
       this.setState({ text: '' });
       e.preventDefault();
@@ -34,17 +34,29 @@ export class NLPDialogScroll extends Component<INLPDialogScrollProps, INLPDialog
   public render() {
     const { nlpDialog } = this.props;
     return (
-      <div styleName="NLPDialog">
-        {nlpDialog && nlpDialog.items.map( (i, idx) => i && <div key={idx} styleName="NLPItem"><span>{i.who}</span>{i.text}</div>)}
-        <div styleName="NLPInput">
-          <textarea
-            spellCheck={false}
-            value={this.state.text}
-            onKeyPress={this.onPressEnter.bind(this)}
-            onChange={ (e: ChangeEvent<HTMLTextAreaElement>) => this.setState({ text: e.target.value }) }
-          />
+      <Fragment>
+        <div styleName="NLPDialog">
+          {nlpDialog && nlpDialog.items.map( (i, idx) => i && <div key={idx} styleName="NLPItem"><span styleName="Circle">{i.who}</span><span styleName="Message">{i.text}</span></div>)}
+          <div styleName="NLPInput">
+            <textarea
+              spellCheck={false}
+              value={this.state.text}
+              onKeyPress={this.onPressEnter.bind(this)}
+              onChange={ (e: ChangeEvent<HTMLTextAreaElement>) => this.setState({ text: e.target.value }) }
+            />
+          </div>
         </div>
-      </div>
+        <svg height="0" width="0">
+          <defs>
+            <clipPath id="left-droplet">
+              <path d="M 10,0 A 10,10 0 0 1 0,10 H 16 V 0 Z" />
+            </clipPath>
+            <clipPath id="right-droplet">
+              <path d="M 6,0 A 10,10 0 0 0 16,10 H 0 V 0 Z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </Fragment>
     );
   }
 }
