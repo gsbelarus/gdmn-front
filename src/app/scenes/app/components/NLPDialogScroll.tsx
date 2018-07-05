80a25776bd7bfd64c908966f07e89dcc556f2f79
andreik <gs1994@gmail.com>
2018-07-05 20:59:28 +0300

    more work on correct items count calculation

--- src/app/scenes/app/components/NLPDialogScroll.tsx
+++ src/app/scenes/app/components/NLPDialogScroll.tsx
@@ -12,35 +12,100 @@ interface INLPDialogScrollProps {
 
 interface INLPDialogScrollState {
   text: string;
+  showFrom: number;
+  showTo: number;
+  recalc: boolean;
 }
 
 @CSSModules(styles)
 export class NLPDialogScroll extends Component<INLPDialogScrollProps, INLPDialogScrollState> {
 
-  state: INLPDialogScrollState = { text: '' };
+  nlpDialogDiv: HTMLDivElement | null = null;
+  shownItems: HTMLDivElement[] = [];
+  state: INLPDialogScrollState;
 
   constructor (props: INLPDialogScrollProps) {
     super(props);
+    const { nlpDialog } = this.props;
+    this.state = {
+      text: '',
+      showFrom: nlpDialog.items.size ? nlpDialog.items.size - 1 : 0,
+      showTo: nlpDialog.items.size ? nlpDialog.items.size - 1 : 0,
+      recalc: true
+    }
+    this.calcVisibleCount = this.calcVisibleCount.bind(this);
   }
 
   private onPressEnter(e: React.KeyboardEvent<HTMLTextAreaElement>) {
     if (e.key === 'Enter' && this.state.text.trim()) {
-      this.props.onSetText(this.state.text);
-      this.setState({ text: '' });
+      const { nlpDialog, onSetText } = this.props;
+      onSetText(this.state.text.trim());
+      this.setState({
+        text: '',
+        showFrom: nlpDialog.items.size ? nlpDialog.items.size - 1 : 0,
+        showTo: nlpDialog.items.size ? nlpDialog.items.size - 1 : 0,
+        recalc: true
+      });
       e.preventDefault();
     }
   }
 
-  public componentDidMount() {
+  private calcVisibleCount() {
+    const { nlpDialog } = this.props;
+    const { showFrom, showTo, recalc } = this.state;
 
+    if (recalc) {
+      if (this.shownItems.length) {
+        if (this.shownItems[0].offsetTop > 38) {
+          if (this.shownItems.length < nlpDialog.items.size && showFrom > 0) {
+            this.setState({ showFrom: showFrom - 1 });
+          } else {
+            this.setState({ recalc: false });
+          }
+        }
+        else if (this.shownItems[0].offsetTop <= 0) {
+          if (showFrom < showTo) {
+            this.setState({
+              showFrom: showFrom + 1,
+              recalc: false
+            });
+          } else {
+            this.setState({ recalc: false });
+          }
+        }
+      } else {
+        this.setState({
+          showFrom: 0,
+          showTo: 0,
+          recalc: false
+        });
+      }
+    }
+  }
+
+  public componentDidMount() {
+    setTimeout( () => this.setState({ recalc: true }), 100);
+  }
+
+  public componentDidUpdate() {
+    this.calcVisibleCount();
   }
 
   public render() {
     const { nlpDialog } = this.props;
+    this.shownItems = [];
     return (
       <Fragment>
-        <div styleName="NLPDialog">
-          {nlpDialog && nlpDialog.items.map( (i, idx) => i && <div key={idx} styleName="NLPItem"><span styleName="Circle">{i.who}</span><span styleName="Message">{i.text}</span></div>)}
+        <div styleName="NLPDialog" ref={ elem => this.nlpDialogDiv = elem }>
+          {
+            nlpDialog && nlpDialog.items.map(
+              (i, idx) => i && typeof idx === 'number' && idx >= this.state.showFrom && idx <= this.state.showTo &&
+                <div key={idx} styleName="NLPItem" ref={ elem => elem && this.shownItems.push(elem) } >
+                  <span styleName="Circle">{i.who}</span>
+                  <span styleName="Message">{i.text}</span>
+                </div>
+            )
+          }
           <div styleName="NLPInput">
             <textarea
               spellCheck={false}
